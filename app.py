af5c4f70b64a589f3eb2e567677492d8a65de7fe
huangwanzi <man.tanawan2546@gmail.com>
2024-01-12 23:39:56 +0700

    first commit

---
+++ app.py
@@ -0,0 +1,19 @@
+import streamlit as st 
+import pandas as pd 
+import plotly.express as px 
+import datetime 
+import time 
+
+#read in the file
+movies_data = pd.read_csv("https://raw.githubusercontent.com/danielgrijalva/movie-stats/7c6a562377ab5c91bb80c405be50a0494ae8e582/movies.csv")
+movies_data.info()
+
+
+
+                   
+    
+                   
+                    
+                   
+                   
+                   
